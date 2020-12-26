b4ef9e4784600125a684d891886c3e35cbd07fe7
joenarus <joe.narus2@gmail.com>
2020-12-26 14:26:25 -0700

    multiple inputs, data table

--- webapp/src/transactions/transactions-page.js
+++ webapp/src/transactions/transactions-page.js
@@ -1,15 +1,21 @@
 import React, { useState, Fragment } from 'react'
 
 const mockTransactions = [
-  { id: 1, amount: '400.00', sender: 'Joe', receiver: 'Best Buy', category: 'Electronics', date: '12/26/2020' }
+  { id: 1, amount: '400.00', user: 'Joe', merchant: 'Best Buy', description: 'Electronics', credit: false, debit: true },
+  { id: 2, amount: '100.00', user: 'Joe', merchant: 'Target', description: 'Return', credit: true, debit: false }
 ]
 
 export function TransactionsPage (props) {
   const [transactions, setTransactions] = useState(mockTransactions)
-  const [currentTransaction, setCurrentTransaction] = useState({})
+  const [currentTransaction, setCurrentTransaction] = useState({ amount: '', user: '', merchant: '', description: '', credit: false, debit: false })
 
-  const handleChange = event => {
-    const transaction = { ...currentTransaction, amount: event.target.value }
+  const handleInputChange = event => {
+    const target = event.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+
+    const name = target.name
+
+    const transaction = { ...currentTransaction, [name]: value }
     setCurrentTransaction(transaction)
   }
 
@@ -18,6 +24,7 @@ export function TransactionsPage (props) {
     const transaction = { ...currentTransaction, id: transactions.length + 1 }
     const newTransactions = [ ...transactions, transaction ]
     setTransactions(newTransactions)
+    setCurrentTransaction({ amount: '', user: '', merchant: '', description: '', credit: false, debit: false })
   }
 
   return (
@@ -25,13 +32,46 @@ export function TransactionsPage (props) {
       <form onSubmit={handleSubmit}>
         <h2>Transactions</h2>
         <h3>Add Transaction</h3>
-        <input onChange={handleChange} type='text' />
+        <label>
+          Amount
+          <input name='amount' onChange={handleInputChange} type='text' value={currentTransaction.amount} />
+        </label>
+        <label>
+          User
+          <input name='user' onChange={handleInputChange} type='text' value={currentTransaction.user} />
+        </label>
+        <label>
+          Merchant
+          <input name='merchant' onChange={handleInputChange} type='text' value={currentTransaction.merchant} />
+        </label>
+        <label>
+          Description
+          <input name='description' onChange={handleInputChange} type='text' value={currentTransaction.description} />
+        </label>
         <input type='submit' value='Save' />
 
         <h3>Past Transactions</h3>
-        {transactions.map(transaction => (
-          <div key={transaction.id}> {transaction.amount} </div>
-        ))}
+        <table>
+          <thead>
+            <tr>
+              <th>User</th>
+              <th>Merchant</th>
+              <th>Description</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map(transaction => (
+              <tr key={transaction.id}>
+                <td>{transaction.user}</td>
+                <td>{transaction.merchant}</td>
+                <td>{transaction.description}</td>
+                <td>{(transaction.credit ? '+' : transaction.debit ? '-' : '') + transaction.amount }</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
       </form>
     </Fragment>
   )
