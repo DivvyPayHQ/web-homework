a5dcd513dedbe652bab6f9d794be331cccf17f2d
Eliott Moreno <eliott_mor@hotmail.com>
2021-01-09 13:57:52 -0700

    added some gibberish when i18n is true as query parameter. and added roman nmerals.

--- webapp/src/views/transactions-table.component.js
+++ webapp/src/views/transactions-table.component.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { css } from '@emotion/core'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
@@ -14,9 +14,43 @@ import { useMutation } from '@apollo/react-hooks'
 import { DELETE_TRANSACTION } from '../graphql/transactions'
 
 // eslint-disable-next-line react/prop-types
-export function TransactionTable ({ transactions }) {
+export function TransactionTable ({ transactions, roman }) {
+  const [i18n] = useState(!window.location.search.includes('false'))
   const creditOrDebit = (credit) => {
     return credit === true ? 'Credit' : 'Debit'
+  }
+
+  const gibberish = '~†bø)‚žá¨H~¶vø&ÂšÑ«Ià6ÈÖŽÞäXKz·c6©ÖþÞÀXP:¼1ÍÔUŸ(˜j†¯"üÊàW>†bì)Þå˜K*·_6¸òŽÅ¤S;}ÓaèiŽ®ä|Ka÷hF®²üuçJ˜7*–Ÿ.è N‰ôfÇjÒ¯±ÆôRÇ}’¡­¸}²¡µ¸w2¦•ºï3ÅÏÏE”3/UÜ?Ð'
+  function getRandomInt (max) {
+    return Math.floor(Math.random() * Math.floor(max))
+  }
+
+  const getGibberish = (i18n, word) => {
+    const wordLength = word.length
+    const gibberishLength = gibberish.length
+    const range = getRandomInt(gibberishLength)
+
+    if (range + wordLength > gibberishLength && range - wordLength > 0) {
+      return i18n === true ? gibberish.substr(range - word.length, range) : word
+    } else if (range - wordLength < 0 && range + wordLength < gibberishLength) {
+      return i18n === true ? gibberish.substr(range, range + word.length) : word
+    } else if (range - wordLength > 0 || range + wordLength < gibberishLength) {
+      return i18n === true ? gibberish.substr(range, range + wordLength) : word
+    }
+    return word
+  }
+
+  function convertToRoman (num, isRoman) {
+    if (!isRoman) { return num }
+    let dict = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 }
+    let roman = ''
+    for (let i in dict) {
+      while (num >= dict[i]) {
+        roman += i
+        num -= dict[i]
+      }
+    }
+    return roman
   }
 
   const [deleteTransaction] = useMutation(DELETE_TRANSACTION)
@@ -26,18 +60,18 @@ export function TransactionTable ({ transactions }) {
       <Table aria-label='simple table' css={tableStyle}>
         <TableHead>
           <TableRow>
-            <TableCell align='left' css={tableHeaderCellStyle}>Amount</TableCell>
-            <TableCell align='left' css={tableHeaderCellStyle}>Credit or Debit</TableCell>
-            <TableCell align='left' css={tableHeaderCellStyle}>Merchant</TableCell>
-            <TableCell align='left' css={tableHeaderCellStyle}>User</TableCell>
-            <TableCell align='center' css={[tableHeaderCellStyle, descriptionCellStyle]}>Description</TableCell>
+            <TableCell align='left' css={tableHeaderCellStyle}>{getGibberish(i18n, 'Amount')}</TableCell>
+            <TableCell align='left' css={tableHeaderCellStyle}>{getGibberish(i18n, 'Credit or Debit')}</TableCell>
+            <TableCell align='left' css={tableHeaderCellStyle}>{getGibberish(i18n, 'Merchant')}</TableCell>
+            <TableCell align='left' css={tableHeaderCellStyle}>{getGibberish(i18n, 'User')}</TableCell>
+            <TableCell align='center' css={[tableHeaderCellStyle, descriptionCellStyle]}>{getGibberish(i18n, 'Description')}</TableCell>
             <TableCell align='center' css={noBorderCell}> </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {transactions.map((transaction) => (
             <TableRow key={transaction.id}>
-              <TableCell align='left' css={noBorderCell}>{transaction.amount}</TableCell>
+              <TableCell align='left' css={noBorderCell}>{convertToRoman(String(transaction.amount), roman)}</TableCell>
               <TableCell align='left' css={noBorderCell}>{creditOrDebit(transaction.credit)}</TableCell>
               <TableCell align='left' css={noBorderCell}>{transaction.merchant.name}</TableCell>
               <TableCell align='left' css={noBorderCell}>{transaction.user.firstName} {transaction.user.lastName}</TableCell>
