9f3bca83d888f489ee8719382d882403eabaf204
Devin Rasmussen <devin.rasmussen@getcanopy.com>
2021-01-24 21:14:26 -0700

    introduce pie charts to visualize user data

--- webapp/src/home/home-page.js
+++ webapp/src/home/home-page.js
@@ -5,11 +5,22 @@ import { GetUsers } from '../gql/users.gql';
 import { TxTable } from '../components/transactions/TxTable'
 import TransactionModal from '../components/transaction-modal/transaction-modal'
 import { Button } from 'rsuite'
+import Visualizations from '../components/visualizations/visualizations';
+import { css } from '@emotion/core';
 
 export function Home () {
   const { loading, error, data = {} } = useQuery(GetTransactions)
-  const {loading: l, error: er, data: {users = []} = {}} = useQuery(GetUsers)
+  const {loading: l, error: er, refetch, data: {users = []} = {}} = useQuery(GetUsers)
   const [transaction, setTransaction] = useState()
+
+const styles = css`
+  display: flex;
+  flex-gap: 30px;
+  .vis-container {
+    flex-basis: 26%;
+    margin-left: 100px;
+  }
+`
 
   if (loading) {
     return (
@@ -29,9 +40,14 @@ export function Home () {
 
   return (
     <Fragment>
-      <Button onClick={() => setTransaction(true)}>add transaction</Button>
-      <TransactionModal close={() => setTransaction()} transaction={transaction} users={users} />
-      <TxTable data={data.transactions} setTransaction={setTransaction} users={users} />
+
+      <TransactionModal close={() => setTransaction()} refetch={refetch} transaction={transaction} users={users} />
+      <div css={styles}>
+        <TxTable data={data.transactions} setTransaction={setTransaction} users={users} />
+        <div className="vis-container">
+          <Visualizations data={data.transactions} users={users} />
+        </div>
+      </div>
     </Fragment>
   );
 }
--- webapp/src/home/index.js
+++ webapp/src/home/index.js
@@ -1,3 +1,1 @@
-import 'rsuite/dist/styles/rsuite-default.css';
-
 export { Home } from './home-page'
