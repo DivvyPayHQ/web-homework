2ecc38895f0cecec41c7cab4779949cccf2f3498
Devin Rasmussen <devin.rasmussen@getcanopy.com>
2021-01-25 02:09:41 -0700

    style and create active class for navbar

--- webapp/src/routes.js
+++ webapp/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
 import { css } from '@emotion/core'
 import { Home } from './home'
 import Users from './users/users.component';
@@ -12,13 +12,13 @@ function AppRouter () {
         <nav css={navStyle}>
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <NavLink activeClassName="is-active" exact to="/">Home</NavLink>
             </li>
             <li>
-              <Link to="/users">Users</Link>
+              <NavLink activeClassName="is-active" to="/users">Users</NavLink>
             </li>
-            <li className='right'>
-              <Link to="/settings">Settings</Link>
+            <li className="right">
+              <NavLink activeClassName="is-active" to="/settings">Settings</NavLink>
             </li>
           </ul>
         </nav>
@@ -46,18 +46,31 @@ const navStyle = css`
   grid-row: 1;
 
   & > ul {
-      display: flex;
-      flex-direction: row;
-      list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    list-style-type: none;
   }
 
   & > ul > li:not(:first-of-type):not(:last-of-type) {
     margin-left: 16px;
   }
+  li > a {
+    text-decoration: none;
+    color: #575757;
+    font-size: 16px;
+  }
+  .is-active {
+    text-decoration: underline
+  }
+  li:hover {
+    background-color: rgba(51, 170, 51, 0.1);
+    transition: background-color 0.5s ease;
+    border-radius: 40%;
+  }
   .right {
     margin-left: auto;
   }
-`
+`;
 
 const contentStyle = css`
   grid-row: 2;
