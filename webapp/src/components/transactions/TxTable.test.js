803b24b8ae3ab6f10d814cddf83884d7db5f0214
Devin Rasmussen <devin.rasmussen@getcanopy.com>
2021-01-25 07:48:31 -0700

    Use MockedProvider by apollo to provide useQuery for txtable

--- webapp/src/components/transactions/TxTable.test.js
+++ webapp/src/components/transactions/TxTable.test.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react'
 import {transactions} from 'mocks/transactions-data.js';
 import { TxTable } from './TxTable';
+import { MockedProvider } from '@apollo/client/testing';
 
 describe('Transactions Table', () => {
   it('should show user "employee4" with amount "150"', () => {
-    render(<TxTable data={transactions} />)
+    render(
+      <MockedProvider>
+        <TxTable data={transactions} users={[]} />
+      </MockedProvider>
+    );
     const row = screen.queryByText('employee4').parentElement.parentElement
     expect(row.textContent.includes('150')).toBeTruthy()
   })
