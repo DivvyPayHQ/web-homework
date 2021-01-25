08db99a9e0537cfc54a2495b9ab4282121f11912
Devin Rasmussen <devin.rasmussen@getcanopy.com>
2021-01-24 21:52:53 -0700

    remove unused Button from react suite

--- webapp/src/home/home-page.js
+++ webapp/src/home/home-page.js
@@ -4,7 +4,6 @@ import { GetTransactions } from '../gql/transactions.gql';
 import { GetUsers } from '../gql/users.gql';
 import { TxTable } from '../components/transactions/TxTable'
 import TransactionModal from '../components/transaction-modal/transaction-modal'
-import { Button } from 'rsuite'
 import Visualizations from '../components/visualizations/visualizations';
 import { css } from '@emotion/core';
 
