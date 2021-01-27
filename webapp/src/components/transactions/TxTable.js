7d1b659845f4d17658f8914a3956ad7e8c41ceae
twzkraus <twzkraus@gmail.com>
2021-01-27 15:55:41 -0800

    add delete transaction functionality

--- webapp/src/components/transactions/TxTable.js
+++ webapp/src/components/transactions/TxTable.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { arrayOf, string, bool, number, shape } from 'prop-types'
 import { css } from '@emotion/core'
+import axios from 'axios'
+const SERVER_URL = 'http://localhost:8000/graphql'
 
 const styles = css`
  .header {
@@ -13,11 +15,23 @@ const styles = css`
  .entry:hover {
    cursor: pointer;
  }
+ .trash-button {
+   color: red;
+ }
 `
 
 const makeDataTestId = (transactionId, fieldName) => `transaction-${transactionId}-${fieldName}`
 
 export function TxTable ({ data, editEntry }) {
+  const mutationQuery = `mutation delete($id:String){delete_article_by_pk(id:$id){id,user_id,merchant_id,description}}`
+
+  const remove = (id) => {
+    axios.post(SERVER_URL, {
+      query: mutationQuery,
+      variables: { id }
+    })
+  }
+
   return (
     <table css={styles}>
       <tbody>
@@ -29,16 +43,17 @@ export function TxTable ({ data, editEntry }) {
           <td >Debit</td>
           <td >Credit</td>
           <td >Amount</td>
+          <td >Delete?</td>
         </tr>
         {
           data.map(tx => {
             const { id, user_id: userId, description, merchant_id: merchantId, debit, credit, amount } = tx
             return (
               <tr
+                className='entry'
                 data-testid={`transaction-${id}`}
                 key={`transaction-${id}`}
-                className='entry'
-                onClick={() => editEntry(id)}
+                onClick={(e) => editEntry(id, e)}
               >
                 <td data-testid={makeDataTestId(id, 'id')}>{id}</td>
                 <td data-testid={makeDataTestId(id, 'userId')}>{userId}</td>
@@ -47,6 +62,7 @@ export function TxTable ({ data, editEntry }) {
                 <td data-testid={makeDataTestId(id, 'debit')}>{debit ? <>&#10003;</> : ''}</td>
                 <td data-testid={makeDataTestId(id, 'credit')}>{credit ? <>&#10003;</> : ''}</td>
                 <td data-testid={makeDataTestId(id, 'amount')}>{amount}</td>
+                <td data-testid={makeDataTestId(id, 'delete')}><button className='trash-button' css={styles} onClick={() => remove(id)}>X</button></td>
               </tr>
             )
           })
