5825f1713ed5f7f950d15393dc94d3c8caa68c80
twzkraus <twzkraus@gmail.com>
2021-01-27 16:38:52 -0800

    add test case for TxTable

--- webapp/src/components/transactions/TxTable.test.js
+++ webapp/src/components/transactions/TxTable.test.js
@@ -1,8 +1,12 @@
 import { render } from '@testing-library/react'
+import { TxTable } from './TxTable'
+import { transactions } from '../../../mocks/transactions-data'
 
 describe('Transactions Table', () => {
   it('should show user "employee4" with amount "150"', () => {
-
+    const {container, getByText} = render(<TxTable data={transactions}/>)
+    expect(getByText('employee4')).toBeInTheDocument()
+    expect(getByText('150')).toBeInTheDocument()
   })
 
 })
