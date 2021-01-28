1d4b43b4ddd5bd9d22257b47a9e58f9480a27b62
twzkraus <twzkraus@gmail.com>
2021-01-28 10:31:49 -0800

    add path to editTransaction with gql

--- webserver/schema/mutations.js
+++ webserver/schema/mutations.js
@@ -3,6 +3,17 @@ const { GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLFloat } = graph
 const { TransactionModel } = require('../data-models/Transaction')
 const TransactionType = require('./transaction-type')
 
+const parseParams = function(user_id, description, merchant_id, debit, credit, amount) {
+  let keys = ['user_id', 'description', 'merchant_id', 'debit', 'credit', 'amount']
+  let result = {}
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i]) {
+      result[keys[i]] = arguments[i]
+    }
+  }
+  return result
+}
+
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
   fields: {
@@ -24,11 +35,28 @@ const mutation = new GraphQLObjectType({
     delete_article_by_pk: {
       type: TransactionType,
       args: {
-        id: { type: GraphQLString },
+        id: { type: GraphQLString }
       },
       /* eslint-disable-next-line camelcase */
-      resolve (parentValue,{ id }) {
+      resolve (parentValue, { id }) {
         return TransactionModel.deleteOne({ _id: id })
+      }
+    },
+    editTransaction: {
+      type: TransactionType,
+      args: {
+        id: { type: GraphQLString },
+        user_id: { type: GraphQLString },
+        description: { type: GraphQLString },
+        merchant_id: { type: GraphQLString },
+        debit: { type: GraphQLBoolean },
+        credit: { type: GraphQLBoolean },
+        amount: { type: GraphQLFloat }
+      },
+      /* eslint-disable-next-line camelcase */
+      resolve (parentValue, { id, user_id, description, merchant_id, debit, credit, amount }) {
+        const params = parseParams(user_id, description, merchant_id, debit, credit, amount)
+        return TransactionModel.updateOne({ _id: id }, { $set: params })
       }
     }
   }
