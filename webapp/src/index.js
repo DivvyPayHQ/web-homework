b5a84258df1b0b177169c21e0fe4ea966737cb14
Jake Badger <jake.badger17@gmail.com>
2021-01-30 14:07:00 -0700

    Fixed apollo provider

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppRouter from './routes'
-import { ApolloProvider } from '@apollo/client'
 import { client } from './network/apollo-client'
 import { StylesProvider } from '@material-ui/core/styles'
-import { ApolloProvider as ReactApolloProvider } from '@apollo/react-hooks'
+import { ApolloProvider } from '@apollo/react-hooks'
 
 ReactDOM.render(
   (
     <div data-app-init=''>
       <ApolloProvider client={client}>
-        <ReactApolloProvider client={client}>
-          <StylesProvider injectFirst>
-            <AppRouter />
-          </StylesProvider>
-        </ReactApolloProvider>
+        <StylesProvider injectFirst>
+          <AppRouter />
+        </StylesProvider>
       </ApolloProvider>
     </div>
   ),
