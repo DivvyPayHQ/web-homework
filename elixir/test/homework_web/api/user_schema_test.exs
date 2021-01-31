cf60bf1e0c6f0638f7d52992f55545ae932cde73
Kelly Felkins <kelly@restlater.com>
2021-01-31 15:59:16 -0800

    adds other user acceptance tests

--- elixir/test/homework_web/api/user_schema_test.exs
+++ elixir/test/homework_web/api/user_schema_test.exs
@@ -1,7 +1,7 @@
 defmodule HomeworkWeb.UserSchemaTest do
   use HomeworkWeb.ConnCase, async: true
 
-  test "gets a list of users", %{conn: conn} do
+  test "get a list of users", %{conn: conn} do
     insert(:user, first_name: "alice", last_name: "allen", tid: "alice")
     insert(:user, first_name: "bobby", last_name: "baker", tid: "bobby")
     insert(:user, first_name: "conor", last_name: "clark", tid: "conor")
@@ -29,7 +29,7 @@ defmodule HomeworkWeb.UserSchemaTest do
            }
   end
 
-  test "creates a user" do
+  test "create a user" do
     company = insert(:company)
 
     mutation = """
@@ -57,6 +57,72 @@ defmodule HomeworkWeb.UserSchemaTest do
              "data" => %{
                "createUser" => %{
                  "dob" => "1998-01-30",
+                 "firstName" => "alice",
+                 "lastName" => "allen"
+               }
+             }
+           }
+  end
+
+  test "update a user" do
+    user = insert(:user, first_name: "lice", last_name: "allen")
+
+    mutation = """
+      mutation updateUser($id: ID, $companyId: ID, $dob: String, $firstName: String, $lastName: String) {
+        updateUser(id: $id, companyId: $companyId, dob: $dob, firstName: $firstName, lastName: $lastName) {
+          dob
+          firstName
+          lastName
+        }
+      }
+    """
+
+    variables = %{
+      "id" => user.id,
+      "companyId" => user.company_id,
+      "dob" => "1998-01-30",
+      "firstName" => "alice",
+      "lastName" => "allen"
+    }
+
+    conn =
+      build_conn()
+      |> post("/", %{"query" => mutation, variables: variables})
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "updateUser" => %{
+                 "dob" => "1998-01-30",
+                 "firstName" => "alice",
+                 "lastName" => "allen"
+               }
+             }
+           }
+  end
+
+  test "delete a user" do
+    user = insert(:user, first_name: "alice", last_name: "allen")
+
+    mutation = """
+      mutation deleteUser($id: ID) {
+        deleteUser(id: $id) {
+          firstName
+          lastName
+        }
+      }
+    """
+
+    variables = %{
+      "id" => user.id
+    }
+
+    conn =
+      build_conn()
+      |> post("/", %{"query" => mutation, variables: variables})
+
+    assert json_response(conn, 200) == %{
+             "data" => %{
+               "deleteUser" => %{
                  "firstName" => "alice",
                  "lastName" => "allen"
                }
