1017bf36389e61dbc5e5242c6bf13ecf5b5fd640
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2021-06-30 10:24:11 -0600

    dockerize apps

--- webserver/index.js
+++ webserver/index.js
@@ -27,7 +27,7 @@ module.exports = function main (options, cb) {
   let serverStarted = false
   let serverClosing = false
 
-  const MONGO_URI = 'mongodb://localhost:27017/graphql'
+  const MONGO_URI = 'mongodb://db:27017/graphql'
 
   mongoose.Promise = global.Promise
   mongoose.connect(MONGO_URI, {
