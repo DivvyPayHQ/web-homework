b9e99862d9949d41020ab309a7d2d86c2fe20647
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2021-07-05 13:37:32 -0600

    fix dash bug

--- webapp/src/app.js
+++ webapp/src/app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 import Transactions from './bundles/transactions/transactions'
 import TransactionsDetail from './bundles/transactions/transactionsDetail'
 import Settings from './bundles/settings/settings'
@@ -8,11 +8,13 @@ import Dashboard from './bundles/dashboard/dashboard'
 export default function App () {
   return (
     <Router>
-      <Route component={Dashboard} exact path='/dashboard' />
-      <Route component={TransactionsDetail} exact path='/transactions/:transactionId' />
-      <Route component={Transactions} exact path='/transactions' />
-      <Route component={Settings} exact path='/settings' />
-      <Redirect to='/dashboard' />
+      <Switch>
+        <Route component={Dashboard} exact path='/dashboard' />
+        <Route component={TransactionsDetail} exact path='/transactions/:transactionId' />
+        <Route component={Transactions} exact path='/transactions' />
+        <Route component={Settings} exact path='/settings' />
+        <Redirect to='/dashboard' />
+      </Switch>
     </Router>
   )
 }
