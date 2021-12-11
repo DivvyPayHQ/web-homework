df52a16f81ba719c43d5844c483dab70c7f82461
Cameron Wengert <cameron.wengert@mx.com>
2021-12-10 20:51:30 -0500

    Boom Box baby.

--- webapp/src/components/music/Player.js
+++ webapp/src/components/music/Player.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react'
-
-// import MidiParser from 'midi-parser-js'
-
-import song from 'src/music/i_want_it_that_way.mid'
-
 import WebAudioFontPlayer from 'webaudiofont'
+import { css } from '@emotion/core'
+import { Button } from '@kyper/button'
+import { TokenContext } from '@kyper/tokenprovider'
+
+import BoomBox from 'src/images/boombox.png'
+
+const songPaths = ['kc-jojo.mid', 'i_want_it_that_way.mid', 'as-long-as-you-love-me.mid']
 
 export class Player extends Component {
   constructor (props) {
     super(props)
+
+    this.resetPlayer()
+
+    this.state = {
+      currentSong: 0,
+      paused: true,
+      loading: false
+    }
+  }
+
+  componentDidMount () {
+    this.loadMidi(this.state.currentSong)
+  }
+
+  componentDidUpdate (_, prevState) {
+    const { currentSong, loading, paused } = this.state
+
+    if (!paused && prevState.paused) {
+      this.play()
+    }
+
+    if (!paused && !loading && prevState.loading) {
+      this.play()
+    }
+
+    if (paused && !prevState.paused) {
+      // Too much work to actually pause it.
+      this.resetPlayer()
+    }
+
+    if (currentSong !== prevState.currentSong) {
+      this.loadMidi(currentSong)
+    }
+  }
+
+  static contextType = TokenContext
+
+  loadMidi = (index = 0) => {
+    (async () => {
+      this.setState({ loading: true })
+      const { default: midi } = await import(`src/music/${songPaths[index]}`)
+      this.resetPlayer()
+      this.parseMidi(midi)
+    })()
+  }
+
+  parseMidi = (midi) => {
+    const happyMidi = Uint8Array.from(window.atob(midi), c => c.charCodeAt(0))
+    const midiFile = new window.MIDIFile(happyMidi)
+    const song = midiFile.parseSong()
+
+    this.setState({ song, loading: false })
+  }
+
+  resetPlayer = () => {
+    // Instance state that we don't want to tie to renders
     this.player = null
     this.audioContext = null
     this.equalizer = null
@@ -20,19 +78,8 @@ export class Player extends Component {
     this.songStart = 0
   }
 
-  componentDidMount () {
-  }
-
-  handleLoadSong = () => {
-    // Parse to a format MIDIFile understands
-    const happySong = Uint8Array.from(window.atob(song), c => c.charCodeAt(0))
-    const midiFile = new window.MIDIFile(happySong)
-    const parsed = midiFile.parseSong()
-
-    this.startLoad(parsed)
-  }
-
-  startLoad = song => {
+  buildPlayer = () => {
+    const { song } = this.state
     const AudioContextFunc = window.AudioContext || window.webkitAudioContext
 
     this.audioContext = new AudioContextFunc()
@@ -51,6 +98,7 @@ export class Player extends Component {
       song.tracks[i].id = nn
       this.player.loader.startLoad(this.audioContext, info.url, info.variable)
     }
+
     for (let i = 0; i < song.beats.length; i++) {
       const nn = this.player.loader.findDrum(song.beats[i].n)
       const info = this.player.loader.drumInfo(nn)
@@ -58,40 +106,47 @@ export class Player extends Component {
       song.beats[i].id = nn
       this.player.loader.startLoad(this.audioContext, info.url, info.variable)
     }
-
-    this.startPlay(song)
   }
 
-  startPlay = (song) => {
-    this.currentSongTime = 0
-    this.songStart = this.audioContext.currentTime
-    this.nextStepTime = this.audioContext.currentTime
+  play = () => {
+    if (!this.currentSongTime) {
+      this.buildPlayer()
+      this.songStart = this.audioContext.currentTime
+      this.nextStepTime = this.audioContext.currentTime
+    }
+
     var stepDuration = 44 / 1000
 
-    this.tick(song, stepDuration)
+    this.tick(this.state.song, stepDuration)
+  }
+
+  next = () => {
+    this.setState(state => {
+      const lastIndex = songPaths.length - 1
+
+      return {
+        currentSong: state.currentSong === lastIndex ? 0 : state.currentSong + 1
+      }
+    })
   }
 
   tick = (song, stepDuration) => {
-    if (this.audioContext.currentTime > this.nextStepTime - stepDuration) {
-      this.sendNotes(song, this.songStart, this.currentSongTime, this.currentSongTime + stepDuration, this.audioContext, this.input, this.player)
-      this.currentSongTime = this.currentSongTime + stepDuration
-      this.nextStepTime = this.nextStepTime + stepDuration
-      if (this.currentSongTime > song.duration) {
-        this.currentSongTime = this.currentSongTime - song.duration
-        this.sendNotes(song, this.songStart, 0, this.currentSongTime, this.audioContext, this.input, this.player)
-        this.songStart = this.songStart + song.duration
+    if (!this.state.paused && !this.state.loading) {
+      if (this.audioContext.currentTime > this.nextStepTime - stepDuration) {
+        this.sendNotes(song, this.songStart, this.currentSongTime, this.currentSongTime + stepDuration, this.audioContext, this.input, this.player)
+        this.currentSongTime = this.currentSongTime + stepDuration
+        this.nextStepTime = this.nextStepTime + stepDuration
+        if (this.currentSongTime > song.duration) {
+          this.currentSongTime = this.currentSongTime - song.duration
+          this.sendNotes(song, this.songStart, 0, this.currentSongTime, this.audioContext, this.input, this.player)
+          this.songStart = this.songStart + song.duration
+        }
       }
-    }
 
-    if (this.nextPositionTime < this.audioContext.currentTime) {
-      // var o = document.getElementById('position');
-      // o.value = 100 * currentSongTime / song.duration;
-      // document.getElementById('tmr').innerHTML = '' + Math.round(100 * currentSongTime / song.duration) + '%';
-      this.nextPositionTime = this.audioContext.currentTime + 3
+      window.requestAnimationFrame(() => {
+        this.tick(song, stepDuration)
+      }, 100)
     }
-    window.requestAnimationFrame(() => {
-      this.tick(song, stepDuration)
-    })
   }
 
   sendNotes = (song, songStart, start, end, audioContext, input, player) => {
@@ -125,10 +180,42 @@ export class Player extends Component {
   }
 
   render () {
+    const tokens = this.context
+
     return (
-      <div>
-        <button onClick={this.handleLoadSong}>DJ, Spin that shit!</button>
+      <div css={containerStyle}>
+        <div css={buttonContainerStyle(tokens)}>
+          <Button onClick={() => this.setState({ paused: !this.state.paused })}>
+            {this.state.paused ? 'PLAY' : 'RESET' }</Button>
+          <Button onClick={this.next}>NEXT</Button>
+        </div>
       </div>
     )
   }
 }
+
+const containerStyle = css`
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
+  width: 500px;
+  height: 300px;
+  background-image: url(${BoomBox});
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const buttonContainerStyle = tokens => css`
+  position: relative;
+  top: 72px;
+
+  & > button {
+    color: ${tokens.Color.Brand300} !important;
+    width: 150px;
+    background-color: black !important;
+    margin-bottom: 4px;
+  }
+`
