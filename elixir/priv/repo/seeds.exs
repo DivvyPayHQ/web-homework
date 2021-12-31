cd0621ac3d68aa441372e67e7cfd84aefefd1179
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2021-12-30 23:00:44 -0500

    #1 Database seed complete
    Add transactions for baby formula, diapers, a guestimate on what a CD in the 90's cost, and a chic-fil-a combo.

--- elixir/priv/repo/seeds.exs
+++ elixir/priv/repo/seeds.exs
@@ -21,21 +21,21 @@ Repo.delete_all(Merchant)
 Repo.delete_all(User)
 
 # Merchant
-Repo.insert!(
+merchant1 = Repo.insert!(
   %Merchant{
     name: "Divvy Mart",
     description: "Where shopping is an experience! *jazz hands*"
   }
 )
 
-Repo.insert!(
+merchant2 = Repo.insert!(
   %Merchant{
     name: "Divvy-fil-a",
     description: "We totally invented the chicken sandwich."
   }
 )
 
-Repo.insert!(
+merchant3 = Repo.insert!(
   %Merchant{
     name: "Divvy Divvi Divvé",
     description: "An American soul/R&B group from Oakland, California, popular during the late 1980s and early to mid-1990s."
@@ -43,7 +43,7 @@ Repo.insert!(
 )
 
 # User
-Repo.insert!(
+user1 = Repo.insert!(
   %User{
     first_name: "Jeremy",
     last_name: "Brayton",
@@ -51,7 +51,7 @@ Repo.insert!(
   }
 )
 
-Repo.insert!(
+user2 = Repo.insert!(
   %User{
     first_name: "Miranda",
     last_name: "Brayton",
@@ -59,7 +59,7 @@ Repo.insert!(
   }
 )
 
-Repo.insert!(
+user3 = Repo.insert!(
   %User{
     first_name: "Clara",
     last_name: "Brayton",
@@ -67,7 +67,7 @@ Repo.insert!(
   }
 )
 
-Repo.insert!(
+user4 = Repo.insert!(
   %User{
     first_name: "Abigail",
     last_name: "Brayton",
@@ -76,3 +76,44 @@ Repo.insert!(
 )
 
 # Transaction
+Repo.insert!(
+  %Transaction{
+    user_id: user1.id,
+    amount: 3958,
+    debit: true,
+    description: "Enfamil NeuroPro Baby Formula, Triple Prebiotic Immune Blend with 2'FL HMO & Expert Recommended Omega-3 DHA, Inspired by Breast Milk, Non-GMO, Refill Box, 31.4 Oz",
+    merchant_id: merchant1.id,
+  }
+)
+
+Repo.insert!(
+  %Transaction{
+    user_id: user2.id,
+    amount: 3528,
+    debit: true,
+    description: "Huggies Snug & Dry Baby Diapers, Size 3, 144 Ct",
+    merchant_id: merchant1.id,
+  }
+)
+
+Repo.insert!(
+  %Transaction{
+    user_id: user1.id,
+    amount: 1698,
+    debit: true,
+    description: "The Revival CD",
+    merchant_id: merchant3.id,
+  }
+)
+
+Repo.insert!(
+  %Transaction{
+    user_id: user2.id,
+    amount: 1059,
+    debit: true,
+    description: "Chic-fil-a Sandwich Combo",
+    merchant_id: merchant3.id,
+  }
+)
+
+# Babies shouldn't be making transactions, unless poopy diapers count
