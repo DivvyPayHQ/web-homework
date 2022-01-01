4bea79690d638824cadf7e55b50d88275c1c3ff3
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2022-01-01 16:45:11 -0500

    #4 Company entities
    Scaffold generated via `mix phx.gen.context Companies Company companies name:string credit_line:integer available_credit:integer --binary-id`. Need to do a little surgery to have it match the other entities.

---
+++ elixir/lib/homework/companies/company.ex
@@ -0,0 +1,21 @@
+defmodule Homework.Companies.Company do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "companies" do
+    field :available_credit, :integer
+    field :credit_line, :integer
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(company, attrs) do
+    company
+    |> cast(attrs, [:name, :credit_line, :available_credit])
+    |> validate_required([:name, :credit_line, :available_credit])
+  end
+end
---
+++ elixir/test/homework/companies_test.exs
@@ -0,0 +1,68 @@
+defmodule Homework.CompaniesTest do
+  use Homework.DataCase
+
+  alias Homework.Companies
+
+  describe "companies" do
+    alias Homework.Companies.Company
+
+    @valid_attrs %{available_credit: 42, credit_line: 42, name: "some name"}
+    @update_attrs %{available_credit: 43, credit_line: 43, name: "some updated name"}
+    @invalid_attrs %{available_credit: nil, credit_line: nil, name: nil}
+
+    def company_fixture(attrs \\ %{}) do
+      {:ok, company} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Companies.create_company()
+
+      company
+    end
+
+    test "list_companies/0 returns all companies" do
+      company = company_fixture()
+      assert Companies.list_companies() == [company]
+    end
+
+    test "get_company!/1 returns the company with given id" do
+      company = company_fixture()
+      assert Companies.get_company!(company.id) == company
+    end
+
+    test "create_company/1 with valid data creates a company" do
+      assert {:ok, %Company{} = company} = Companies.create_company(@valid_attrs)
+      assert company.available_credit == 42
+      assert company.credit_line == 42
+      assert company.name == "some name"
+    end
+
+    test "create_company/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Companies.create_company(@invalid_attrs)
+    end
+
+    test "update_company/2 with valid data updates the company" do
+      company = company_fixture()
+      assert {:ok, %Company{} = company} = Companies.update_company(company, @update_attrs)
+      assert company.available_credit == 43
+      assert company.credit_line == 43
+      assert company.name == "some updated name"
+    end
+
+    test "update_company/2 with invalid data returns error changeset" do
+      company = company_fixture()
+      assert {:error, %Ecto.Changeset{}} = Companies.update_company(company, @invalid_attrs)
+      assert company == Companies.get_company!(company.id)
+    end
+
+    test "delete_company/1 deletes the company" do
+      company = company_fixture()
+      assert {:ok, %Company{}} = Companies.delete_company(company)
+      assert_raise Ecto.NoResultsError, fn -> Companies.get_company!(company.id) end
+    end
+
+    test "change_company/1 returns a company changeset" do
+      company = company_fixture()
+      assert %Ecto.Changeset{} = Companies.change_company(company)
+    end
+  end
+end
