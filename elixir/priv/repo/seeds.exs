0c2649999b71ea6a7f1ad366988a68b9772cbc93
Chad Jemmett <cjemmett@wehco.com>
2022-01-04 20:00:49 -0600

    Can add all the elements. Just need to combine them in a way so they inherit each other

--- elixir/priv/repo/seeds.exs
+++ elixir/priv/repo/seeds.exs
@@ -16,6 +16,9 @@
 defmodule Homework.DatabaseSeeder do
   alias Homework.Companies.Company
   alias Homework.Repo
+  alias Homework.Users.User
+  alias Homework.Merchants.Merchant
+  alias Homework.Transactions.Transaction
 
 
   def insert_company do
@@ -26,11 +29,35 @@ defmodule Homework.DatabaseSeeder do
     }
   end
 
-  def print_the_name(company) do
-    IO.puts company.name
+  def insert_user(company) do
+
+    Repo.insert! %User{
+      first_name: Faker.Person.first_name(),
+      last_name: Faker.Person.last_name(),
+      dob: Date.to_string(Faker.Date.date_of_birth(1970 .. 1990)),
+      company_id: company.id
+    }
+  end
+
+  def insert_merchant do
+    Repo.insert! %Merchant{
+      description: Faker.Lorem.sentence(6 .. 10),
+      name: Faker.Commerce.En.department()
+    }
+  end
+
+  def insert_transaction(merchant, user) do
+    Repo.insert! %Transaction{
+      amount: :rand.uniform(100),
+      credit: true,
+      debit: false,
+      description: Faker.Commerce.En.department(),
+      merchant_id: merchant.id
+      user_id: user.id,
+    }
   end
 
 
 end
 
-(1 .. 5) |> Enum.each(fn _ -> Homework.DatabaseSeeder.insert_company |> Homework.DatabaseSeeder.print_the_name() end)
+(1 .. 5) |> Enum.each(fn _ -> Homework.DatabaseSeeder.insert_company |> Homework.DatabaseSeeder.create_user() |> new_merechant_transaction()  end)
